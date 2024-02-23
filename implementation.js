677b90cc558b52e0268f0ab1f833b15aec2d1e89
moneem HENCHIR <moneem.henchir@leadwire.io>
2024-02-23 16:43:22 +0100

    remove option

--- implementation.js
+++ implementation.js
@@ -1,19 +1,42 @@
 var organicapi = class extends ExtensionCommon.ExtensionAPI {
-    getAPI(context) {
-      return {
-        organicapi: {
-          async organicPrefs() {
-            let pref = Services.prefs.getCharPref("mail.compose.other.header");
-            if (!pref.split(',').includes('X-Expediteur')) {
-                Services.prefs.setCharPref("mail.compose.other.header", pref+",X-Expediteur");
-            }
-
-            Services.prefs.setCharPref("mail.extensions.messagerieorganique.", "localhost");
-          },
-          async getOrganicAccount() {
-            
+  getAPI(context) {
+    return {
+      organicapi: {
+        async organicPrefs() {
+          let pref = Services.prefs.getCharPref("mail.compose.other.header");
+          if (!pref.split(',').includes('X-Expediteur')) {
+            Services.prefs.setCharPref("mail.compose.other.header", pref + ",X-Expediteur");
           }
+
+          Services.prefs.setCharPref("mail.extensions.messagerieorganique.", "localhost");
+        },
+        async getOrganicAccounts() {
+          let accounts = Services.prefs.getCharPref("mail.accountmanager.accounts").split(',');
+          let id = "";
+          let hostname = "";
+          let hostnameOrganique = ""
+          let accountsOrganique = [];
+          let organiqueName = "";
+          accounts.forEach(accountId => {
+            let server = Services.prefs.getCharPref("mail.account." + accountId + ".server");
+            try {
+              id = Services.prefs.getCharPref("mail.account." + accountId + ".identities");
+              hostname = Services.prefs.getCharPref("mail.server." + server + ".hostname");
+              hostnameOrganique = Services.prefs.getCharPref("extensions.changetreeviewdisplay.msgOrganique");
+              organiqueName = Services.prefs.getCharPref("extensions.changetreeviewdisplay.msgOrganiqueName");
+              if (hostname == hostnameOrganique) {  
+                accountsOrganique.push({ "email": Services.prefs.getCharPref("mail.identity." + id + ".useremail") });
+              }
+
+            } catch {
+             }
+            
+          });
+
+          let result = { "name": organiqueName, "emails": accountsOrganique };
+          return result;
         }
       }
     }
-  };
+  }
+};
--- msgorganic.js
+++ msgorganic.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
-  renderItemsFromLocalStorage();
+  renderItemsFromPrefs();
 
   document.getElementById('itemList').addEventListener('click', function (event) {
     if (event.target.tagName === 'LI') {
@@ -9,28 +9,29 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  function renderItemsFromLocalStorage() {
-    var itemList = getItemListFromLocalStorage();
+  async function renderItemsFromPrefs() {
+    var itemList = await getItemListFromPrefs();
     var listContainer = document.getElementById('itemList');
     listContainer.innerHTML = "";
+    console.log(itemList)
     if (itemList.length === 0) {
       var li = document.createElement('h5');
       li.textContent = "Configurer les adresses organiques";
       listContainer.appendChild(li);
     }
-    itemList.forEach(function (item, index) {
+    itemList.emails.forEach(function (item ) {
       var li = document.createElement('li');
-      li.setAttribute('data-item', item.name);
-      li.textContent = item.name;
+      li.setAttribute('data-item', item.email);
+      li.textContent = item.email;
       listContainer.appendChild(li);
     });
 
 
   }
 
-  function getItemListFromLocalStorage() {
-    var storedItems = localStorage.getItem('organicList');
-    return storedItems ? JSON.parse(storedItems) : [];
+  async function getItemListFromPrefs() {
+    let emailsOrganic = await messenger.organicapi.getOrganicAccounts();
+    return emailsOrganic;
   }
 
 
@@ -49,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let details = await messenger.compose.getComposeDetails(tabs[0].id);
     await addReplyTo(details, tabs[0].id, item);
     await addExpediteur(details, tabs[0].id);
-    await changeFrom(details, tabs[0].id);
+    await changeFrom(details, tabs[0].id,item);
   }
 
 
@@ -71,24 +72,19 @@ document.addEventListener('DOMContentLoaded', function () {
     let account = await messenger.identities.getDefault('account1');
     if (!details.customHeaders.includes({ "X-Expediteur": account.email })) {
       details.customHeaders.push({ "name": "X-Expediteur", "value": account.email });
-      console.log({ modifiedDetails: details });
-      let setDetails = await messenger.compose.setComposeDetails(tabId, details);
-      console.log({ setDetails });
-      let readBackDetails = await messenger.compose.getComposeDetails(tabId);
-      console.log({ readBackDetails });
+      // details.cc.push("test@test.com");
+      console.log(details);
     }
-  }
-  async function changeFrom(details, tabId) {
-    // get accountId of email "Messagerie Organique" 
-    let acc = messenger.organicapi.getOrganicAccount();
-    let account = await messenger.identities.getDefault('account3');
-    let email = "<" + account.email + ">";
-    // get Name of email "Messagerie Organique"
-    let name = "Messagerie Organique ";
-    details.from = name + email;
     await messenger.compose.setComposeDetails(tabId, details);
-    var pref = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefBranch);
-    console.log(pref)
+
+    let afterModify = await messenger.compose.getComposeDetails(tabId);
+    console.log({ afterModify });  }
+  async function changeFrom(details, tabId, item) {
+    let email = "<" + item + ">";
+    // get Name of email "Messagerie Organique"
+    let el = await getItemListFromPrefs();
+    details.from = el.name + email;
+    await messenger.compose.setComposeDetails(tabId, details);
   }
 
 });
